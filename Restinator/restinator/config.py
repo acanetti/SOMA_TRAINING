3f4d565bdf2a364c37008a92c1c9e76c279c91c7
acanetti <47980384+acanetti@users.noreply.github.com>
2023-02-20 17:05:35 +0100

    Add files via upload

---
+++ Restinator/restinator/config.py
@@ -0,0 +1,7 @@
+import os
+SECRET_KEY = '#d#JCqTTW\nilK\\7m\x0bp#\tj~#H'
+
+
+# Database initialization
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
---
+++ Restinator/restinator/run.py
@@ -0,0 +1,5 @@
+import restinator
+from restinator import app
+
+if __name__ == "__main__":
+        app.run(host="0.0.0.0",port='3838',debug=True)
